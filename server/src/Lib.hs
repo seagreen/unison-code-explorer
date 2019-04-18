446668d8c05944bc3b90176b7feb32debb2f5351
Ian Grant Jeffries <ian@housejeffries.com>
2019-04-18 19:47:40 -0400

    Don't run the unison editor.

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -2,44 +2,39 @@ module Lib where
 
 import Control.Monad (when)
 import Prelude
-import Safe (headMay)
-import System.Environment (getArgs)
+import Unison.Codebase (Codebase)
 import Unison.Codebase.Serialization.V0 (formatSymbol)
 import Unison.Parser (Ann(External))
+import Unison.Symbol (Symbol)
 
 import qualified Unison.Codebase.Editor as Editor
 import qualified Unison.Codebase.FileCodebase as FileCodebase
 import qualified Unison.Codebase.Serialization as S
-import qualified Unison.CommandLine.Main as CommandLine
-import qualified Unison.Runtime.Rt1IO as Rt1
 
 data Config = Config Bool String deriving Show
 
 run :: Config -> IO ()
 run _ = do
-  args <- getArgs
-  -- hSetBuffering stdout NoBuffering -- cool
-  let codebasePath  = ".unison"
-      initialBranchName = "master"
-      scratchFilePath   = "."
-      theCodebase =
-        FileCodebase.codebase1 External formatSymbol formatAnn codebasePath
-      launch = CommandLine.main
-        scratchFilePath
-        initialBranchName
-        (headMay args)
-        (pure Rt1.runtime)
-        theCodebase
+  let
+    codebasePath :: FilePath
+    codebasePath =
+      ".unison"
+
+    theCodebase :: Codebase IO Symbol Ann
+    theCodebase =
+      FileCodebase.codebase1 External formatSymbol formatAnn codebasePath
+
   exists <- FileCodebase.exists codebasePath
   if exists
     then do
       Editor.initializeCodebase theCodebase
-      launch
+      putStrLn "Success"
     else
       putStrLn "No codebase found"
 
 formatAnn :: S.Format Ann
-formatAnn = S.Format (pure External) (\_ -> pure ())
+formatAnn =
+  S.Format (pure External) (\_ -> pure ())
 
 one :: Int
 one =
